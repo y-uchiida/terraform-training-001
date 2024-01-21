c1855615b48b53e4bcbc8ac4d1343bd2e822813f
yugo uchiida <y.uchiida@nirneege.com>
2024-01-22 03:57:47 +0900

    Appサーバー用と運用管理用のセキュリティグループの設定を追加 - App サーバー用はセキュリティグループの作成のみ - 運用管理は http/https と ssh, tcp3000を設定

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -88,3 +88,133 @@ resource "aws_security_group_rule" "SecurityGroup_webServer_outbound_tcp3000" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
+# app サーバー用のセキュリティグループ
+# resource "aws_security_group" "識別名"
+resource "aws_security_group" "SecurityGroup_appServer" {
+
+  # セキュリティグループの名前を設定
+  name = "${var.projectName}-${var.environment}-SecurityGroup_appServer"
+
+  # セキュリティグループの説明を設定
+  description = "security group for app servers"
+
+  # VPC の ID を設定
+  vpc_id = aws_vpc.vpc.id
+
+  # セキュリティグループのタグを設定
+  tags = {
+    Name    = "${var.projectName}-${var.environment}-SecurityGroup_appServer"
+    Project = var.projectName
+    Env     = var.environment
+  }
+}
+
+# 運用管理用のセキュリティグループ
+# resource "aws_security_group" "識別名"
+resource "aws_security_group" "SecurityGroup_operationManagement" {
+
+  # セキュリティグループの名前を設定
+  name = "${var.projectName}-${var.environment}-SecurityGroup_operationManagement"
+
+  # セキュリティグループの説明を設定
+  description = "security group for operationManagement"
+
+  # VPC の ID を設定
+  vpc_id = aws_vpc.vpc.id
+
+  # セキュリティグループのタグを設定
+  tags = {
+    Name    = "${var.projectName}-${var.environment}-SecurityGroup_operationManagement"
+    Project = var.projectName
+    Env     = var.environment
+  }
+}
+
+# 運用管理用のセキュリティグループのインバウンドルールを設定 - ssh
+# resource "aws_security_group_rule" "識別名"
+resource "aws_security_group_rule" "SecurityGroup_operationManagement_inbound_ssh" {
+
+  # 接続するセキュリティグループのIDを設定
+  security_group_id = aws_security_group.SecurityGroup_operationManagement.id
+
+  # インバウンドルールのタイプを設定
+  type = "ingress"
+
+  # インバウンドルールのプロトコルを設定
+  protocol = "tcp"
+
+  # インバウンドルールのポート番号を設定
+  from_port = 22
+  to_port   = 22
+
+  # インバウンドルールのソースを設定
+  # CIDR で指定
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+# 運用管理用のセキュリティグループのインバウンドルールを設定 - tcp3000
+# resource "aws_security_group_rule" "識別名"
+resource "aws_security_group_rule" "SecurityGroup_operationManagement_inbound_tcp3000" {
+
+  # 接続するセキュリティグループのIDを設定
+  security_group_id = aws_security_group.SecurityGroup_operationManagement.id
+
+  # インバウンドルールのタイプを設定
+  type = "ingress"
+
+  # インバウンドルールのプロトコルを設定
+  protocol = "tcp"
+
+  # インバウンドルールのポート番号を設定
+  from_port = 3000
+  to_port   = 3000
+
+  # インバウンドルールのソースを設定
+  # CIDR で指定
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+
+# 運用管理用のセキュリティグループのアウトバウンドルールを設定 - http
+# resource "aws_security_group_rule" "識別名"
+resource "aws_security_group_rule" "SecurityGroup_operationManagement_outbound_http" {
+
+  # 接続するセキュリティグループのIDを設定
+  security_group_id = aws_security_group.SecurityGroup_operationManagement.id
+
+  # アウトバウンドルールのタイプを設定
+  type = "egress"
+
+  # アウトバウンドルールのプロトコルを設定
+  protocol = "tcp"
+
+  # アウトバウンドルールのポート番号を設定
+  from_port = 80
+  to_port   = 80
+
+  # アウトバウンドルールのソースを設定
+  # CIDR で指定
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+# 運用管理用のセキュリティグループのアウトバウンドルールを設定 - https
+# resource "aws_security_group_rule" "識別名"
+resource "aws_security_group_rule" "SecurityGroup_operationManagement_outbound_https" {
+
+  # 接続するセキュリティグループのIDを設定
+  security_group_id = aws_security_group.SecurityGroup_operationManagement.id
+
+  # アウトバウンドルールのタイプを設定
+  type = "egress"
+
+  # アウトバウンドルールのプロトコルを設定
+  protocol = "tcp"
+
+  # アウトバウンドルールのポート番号を設定
+  from_port = 443
+  to_port   = 443
+
+  # アウトバウンドルールのソースを設定
+  # CIDR で指定
+  cidr_blocks = ["0.0.0.0/0"]
+}
