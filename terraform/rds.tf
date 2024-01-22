2ba6d200527c94654bc7b7d1ba9a46d2a14c7bb4
yugo uchiida <y.uchiida@nirneege.com>
2024-01-23 03:17:13 +0900

    RDSインスタンスの設定を追加

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -46,3 +46,75 @@ resource "random_string" "dbPassword" {
   length  = 16
   special = false
 }
+
+# RDSの設定
+# インスタンスタイプはt3.micro
+# ストレージタイプはgp2
+# ストレージ容量は20GBで、最大50GBまで自動拡張
+# マルチAZは利用しない
+# アベイラビリティゾーンはap-northeast-1aとap-northeast-1aを利用
+resource "aws_db_instance" "mySqlStandAlone" {
+  engine         = "mysql"
+  engine_version = "8.0"
+  port           = 3306
+  identifier     = "${var.projectName}-${var.environment}-mysql-standalone"
+
+  username = "admin"
+  password = random_string.dbPassword.result
+
+  # DB名はプロジェクト名と環境名を組み合わせて作成
+  # ハイフンが利用できないため、アンダースコアを利用する
+  name = "${var.projectName}_${var.environment}"
+
+  instance_class = "db.t3.micro"
+
+  storage_type          = "gp2"
+  allocated_storage     = 20
+  max_allocated_storage = 50
+  storage_encrypted     = false
+
+  # マルチAZは利用しない
+  # アベイラビリティゾーンはap-northeast-1aとap-northeast-1aを利用
+  multi_az             = false
+  availability_zone    = "ap-northeast-1a"
+  db_subnet_group_name = aws_db_subnet_group.mySqlStandAloneSubnetGroup.name
+
+  # 割当するセキュリティグループを設定
+  vpc_security_group_ids = [
+    aws_security_group.SecurityGroup_databaseServer.id
+  ]
+  # 外部アクセスを拒否する(インターネットからの直接接続ができない)
+  publicly_accessible = false
+
+  # パラメータグループとオプショングループを設定する
+  parameter_group_name = aws_db_parameter_group.mySqlStandAloneParameterGroup.name
+  option_group_name    = aws_db_option_group.mySqlStandAloneOptionGroup.name
+
+  # バックアップ設定
+  # バックアップ実施時刻は 4:00-5:00 AM
+  # バックアップ保持期間は 7日間
+  backup_window           = "04:00-05:00"
+  backup_retention_period = 7
+
+  # メンテナンスウィンドウは 月曜日 5:00-7:00 AM
+  # この時間中は、RDSインスタンスが停止する可能性がある
+  # RDSのマイナーバージョンアップは自動で実施する
+  maintenance_window         = "mon:05:00-mon:07:00"
+  auto_minor_version_upgrade = true
+  apply_immediately          = true
+
+  # 削除保護を無効化
+  # 本番環境など、誤って削除しないようにする場合はtrueにする
+  deletion_protection = false
+
+  # 最終スナップショットを作成しない
+  # 本番環境など、復旧が行えるようにする必要がある場合はfalseにする
+  skip_final_snapshot = true
+
+  # タグを設定
+  tags = {
+    Name    = "${var.projectName}-${var.environment}-mysql-standalone"
+    Project = var.projectName
+    Env     = var.environment
+  }
+}
